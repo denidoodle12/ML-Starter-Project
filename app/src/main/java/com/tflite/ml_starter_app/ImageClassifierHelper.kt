5a7518f4fb957c7b533a791015ae639efbf9510f
Kamvret <kamvretgamingyt@gmail.com>
2024-10-31 09:18:41 +0700

    Add Implement GPU Provide by Google Service to Best Performance

--- app/src/main/java/com/tflite/ml_starter_app/ImageClassifierHelper.kt
+++ app/src/main/java/com/tflite/ml_starter_app/ImageClassifierHelper.kt
@@ -2,19 +2,24 @@ package com.tflite.ml_starter_app
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.SystemClock
 import android.util.Log
 import android.view.Surface
 import androidx.camera.core.ImageProxy
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
 import org.tensorflow.lite.DataType
+import org.tensorflow.lite.gpu.CompatibilityList
 import org.tensorflow.lite.support.common.ops.CastOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.task.core.BaseOptions
 import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
-import org.tensorflow.lite.task.vision.classifier.Classifications
-import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+import org.tensorflow.lite.task.gms.vision.TfLiteVision
+import org.tensorflow.lite.task.gms.vision.classifier.Classifications
+import org.tensorflow.lite.task.gms.vision.classifier.ImageClassifier
 
 class ImageClassifierHelper(
     private var threshold: Float = 0.1f,
@@ -26,20 +31,57 @@ class ImageClassifierHelper(
     // Initialization object of ImageClassifier
     private var imageClassifier: ImageClassifier? = null
     init {
-        setupImageClassifier()
+        // GPU Delegate adalah driver yang memungkinkan TensorFlow Lite untuk mengakses hardware GPU.
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable ->
+            val optionsBuilder = TfLiteInitializationOptions.builder()
+            if (gpuAvailable) {
+                optionsBuilder.setEnableGpuDelegateSupport(true) // Untuk mengaktifkan GPU Delegate
+            }
+            // inisialisasi
+            TfLiteVision.initialize(context, optionsBuilder.build())
+        }.addOnSuccessListener {
+            setupImageClassifier()
+        }.addOnFailureListener {
+            classifierListener?.onError(context.getString(R.string.tflitevision_is_not_initialized_yet))
+        }
     }
 
-    /*Setup Configuration*/
+    /*Setup Configuration with
+     - ImageClassifierOption
+     - BaseOption
+    */
     private fun setupImageClassifier() {
         val optionBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            // menentukan batas minimal keakuratan dari hasil yang ditampilkan. 0,1 artinya 10%
             .setScoreThreshold(threshold)
+            // menentukan batas maksimal jumlah yang dihasilkan
             .setMaxResults(maxResults)
         val baseOptionsBuilder = BaseOptions.builder()
-            .setNumThreads(4)
+
+            if (CompatibilityList().isDelegateSupportedOnThisDevice) {
+                baseOptionsBuilder.useGpu()
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1){
+                baseOptionsBuilder.useNnapi()
+            } else {
+                // Menggunakan CPU
+                baseOptionsBuilder.setNumThreads(4)
+            }
+
+        // menentukan jumlah thread yang digunakan untuk melakukan referensi
+        // .setNumThreads(4)
         optionBuilder.setBaseOptions(baseOptionsBuilder.build())
 
         /*Menyertakan Model dan Konfigurasi*/
         try {
+            if (!TfLiteVision.isInitialized()) {
+                val errorMessage = context.getString(R.string.tflitevision_is_not_initialized_yet)
+                Log.e(TAG, errorMessage)
+                classifierListener?.onError(errorMessage)
+                return
+            }
+
+
+            // membuat image classifier berdasarkan assetfilemodel dan option yg didefinisikan sebelumnya
             imageClassifier = ImageClassifier.createFromFileAndOptions(
                 context,
                 modelName,
@@ -60,6 +102,8 @@ class ImageClassifierHelper(
         /* Image Processor
         Disini menyiapkan image processor untuk melakukan preprocessing pada gambar
         sesuai dengan metadata pada model menggunakan ukuran 224x224 dan tipe data UINT8
+        preprocessing itu sendiri merupakan persiapan sebelum gambar diproses supaya sesuai dengan model,
+        seperti mengubah ukuran, memutar, dan mengkonversi gambar.
         */
         val imageProcessor = ImageProcessor.Builder()
             .add(ResizeOp(224,224,ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
@@ -99,6 +143,7 @@ class ImageClassifierHelper(
     }
 
     private fun toBitmap(image: ImageProxy): Bitmap {
+
         val bitmapBuffer = Bitmap.createBitmap(
             image.width,
             image.height,
