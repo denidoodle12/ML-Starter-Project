1a5ad38107679ec3f63a1b0ece66ba7613d683f9
Kamvret <kamvretgamingyt@gmail.com>
2024-10-31 12:00:59 +0700

    Implement Object Detection with CustomView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.tensorflow.lite.support)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
